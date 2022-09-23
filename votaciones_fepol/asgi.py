ca63ab6c047548af08789a96a1e410d859c409fb
Erick Lino <eri.sion@live.com>
2022-09-22 21:19:19 -0500

    initial commit

---
+++ votaciones_fepol/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for votaciones_fepol project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'votaciones_fepol.settings')
+
+application = get_asgi_application()
